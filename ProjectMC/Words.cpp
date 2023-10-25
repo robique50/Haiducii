02a11c1b03d18f069e5cfc7b7293750e85a839c7
adrixul <147802632+adrixul@users.noreply.github.com>
2023-10-25 18:57:12 +0300

    functiile de generare cuvinte

--- ProjectMC/Words.cpp
+++ ProjectMC/Words.cpp
@@ -9,3 +9,14 @@ void Words::addWord(const std::string& word)
 {
 	words.push_back(word);
 }
+
+std::string Words::getRandomWord()
+{
+    if (words.empty()) {
+        return "No words available.";
+    }
+
+    int randomIndex = std::rand() % words.size();
+    return words[randomIndex];
+}
+
--- ProjectMC/Words.h
+++ ProjectMC/Words.h
@@ -11,7 +11,6 @@ public:
 	Words();
 	void addWord(const std::string& word);
 	std::string getRandomWord();
-
 private:
 	std::vector<std::string> words;
 };
