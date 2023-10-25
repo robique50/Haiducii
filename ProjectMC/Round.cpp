c8531ad95fe0d18ce1ceb9c62d8df23eba6822a5
Haiduc_Robert <robert.haiduc@student.unitbv.ro>
2023-10-25 21:10:21 +0300

    Class Round with a function for score

--- ProjectMC/Round.cpp
+++ ProjectMC/Round.cpp
@@ -13,4 +13,31 @@ bool Round::CorrectGuess(const std::string & guess)
             return std::tolower(a) == std::tolower(b);
         });
 }
+void Round::AddGuess(const std::string& guess, int responseTime)
+{
+    m_guesses.push_back(std::make_pair(guess, responseTime));
 
+
+}
+int Round::CalculateScore() {
+    if (m_guesses.empty()) {
+        return -100;
+    }
+    double totalResponseTime = 0;
+    int numCorrectGuesses = 0;
+    for (const auto& guess : m_guesses) {
+        if (guess.first == m_currentWord) {
+            totalResponseTime += guess.second;
+            numCorrectGuesses++;
+        }
+    }
+
+    if (numCorrectGuesses > 0) {
+        double averageResponseTime = totalResponseTime / numCorrectGuesses;
+        int drawerScore = static_cast<int>((60 - averageResponseTime) / 60.0 * 100);
+        return drawerScore;
+    }
+    else {
+        return -100; 
+    }
+}
