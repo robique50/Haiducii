8ce64a0e2e278ca1466841b0607497248ed0a278
Valy2255 <ghitavalentin70@yahoo.com>
2023-11-04 21:08:41 +0200

    second commit for User module

--- ProjectMC/User.cpp
+++ ProjectMC/User.cpp
@@ -1,14 +1,48 @@
 import user;
+import <iostream>;
 using skribbl::User;
-User::User(const std::string& username, bool isDrawing) :m_username(username), m_score(0), m_isDrawing(isDrawing)
-{
+using skribbl::Statistics;
+
+Statistics::Statistics() : totalScore(0), averageScore(0), averageTime(0), gamesPlayed(0) {}
+
+int Statistics::getTotalScore() const {
+    return totalScore;
 }
 
-std::string User::getUsername()
-{
-	return m_username;
+double Statistics::getAverageScore() const {
+    return averageScore;
 }
 
-int16_t User::getScore()
-{
-	return m_score;
+double Statistics::getAverageTime() const {
+    return averageTime;
+}
+
+void Statistics::updateStatistics(int score, double time) {
+    totalScore += score;
+    averageScore = static_cast<double>(totalScore) / gamesPlayed;
+    averageTime = (averageTime * gamesPlayed + time) / (gamesPlayed + 1);
+    gamesPlayed++;
+}
+
+User::User(const std::string& username, const std::string& password, const std::string& avatar, bool isDrawing)
+    : m_username(username), m_password(password), m_avatar(avatar), m_isDrawing(isDrawing) {}
+
+std::string User::getUsername() const {
+    return m_username;
+}
+
+std::string User::getPassword() const {
+    return m_password;
+}
+
+std::string User::getAvatar() const {
+    return m_avatar;
+}
+
+int16_t User::getScore() const {
+    return m_statistics.getTotalScore();
+}
+
+void User::updateStatistics(int score, double time) {
+    m_statistics.updateStatistics(score, time);
+}
--- ProjectMC/user.cppm
+++ ProjectMC/user.cppm
@@ -1,19 +1,39 @@
 export module user;
-#include <iostream>;
-
+import <iostream>;
+import <string>;
 namespace skribbl
 {
+
+	export class Statistics {
+	public:
+		Statistics();
+		int getTotalScore() const;
+		double getAverageScore() const;
+		double getAverageTime() const;
+		void updateStatistics(int score, double time);
+	private:
+		int totalScore;
+		double averageScore;
+		double averageTime;
+		int gamesPlayed;
+	};
+
 	export class User
 	{
 	public:
-		User(const std::string& username, bool isDrawing);
-		std::string getUsername();
-		int16_t getScore();
+		User(const std::string& username, const std::string& password, const std::string& avatar, bool isDrawing);
+		std::string getUsername() const;
+		std::string getPassword() const;
+		std::string getAvatar() const;
+		int16_t getScore() const;
+		void updateStatistics(int score, double time);
 	private:
 		std::string m_username;
-		int16_t m_score;
+		std::string m_password;
+		std::string m_avatar;
 		bool m_isDrawing;
-
+		Statistics m_statistics;
 	};
 
+	
 }
