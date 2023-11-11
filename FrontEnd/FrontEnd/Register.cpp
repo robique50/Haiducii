a78dcbbcc3f7b0c73ce56a963cb53c1ad44a4e05
Valy2255 <ghitavalentin70@yahoo.com>
2023-11-11 14:14:06 +0200

    Added Register UI

--- FrontEnd/FrontEnd/Register.cpp
+++ FrontEnd/FrontEnd/Register.cpp
@@ -8,3 +8,16 @@ Register::Register(QWidget *parent)
 
 Register::~Register()
 {}
+
+void Register::on_pushButton_Login_now_clicked()
+{
+
+}
+
+void Register::on_pushButton_Register_clicked()
+{
+	QString fullname,username, password;
+	fullname = ui.lineEdit_FullName->text();
+	username = ui.lineEdit_username->text();
+	password = ui.lineEdit_password->text();
+}
--- FrontEnd/FrontEnd/Register.h
+++ FrontEnd/FrontEnd/Register.h
@@ -12,6 +12,9 @@ public:
 	Register(QWidget *parent = nullptr);
 	~Register();
 
+private slots:
+	void on_pushButton_Login_now_clicked();
+	void on_pushButton_Register_clicked();
 private:
 	Ui::RegisterClass ui;
 };
