15332aea58b441f48dfce966f832e3dfacfe7a7c
Haiduc_Robert <robert.haiduc@student.unitbv.ro>
2023-11-19 21:22:47 +0200

    Added word to the server

--- ProjectMC/server.cpp
+++ ProjectMC/server.cpp
@@ -1,12 +1,22 @@
 #include "server.h"
-
+#include "Database.h"
+import user;
 void serverHandle()
 {
 	crow::SimpleApp app;
-
+	useDatabase();
 	CROW_ROUTE(app, "/")([]()
 		{
 			return "The server is running ";
+		});
+
+	CROW_ROUTE(app, "/register/<string>")
+		([db&]
+		() 	
+		{
+			using namespace sqlite_orm; // fara asta nu merg operatorii pt conditii (==, < etc)
+			auto rows = db.select(sql::columns(&Word::word);
+		);
 		});
 
 	
