070828ede80d209759a35645d44bb1df51cafda7
adrixul <147802632+adrixul@users.noreply.github.com>
2023-12-03 20:20:44 +0200

    difficultylevel fucntions to determine diffrent difficulties

--- ProjectMC/difficultyLevel.h
+++ ProjectMC/difficultyLevel.h
@@ -14,6 +14,20 @@ class difficultyLevel
     Level getLevel() const {
         return level;
     }
+
+    bool isDifficult() const {
+        return level == Hard;
+    }
+
+    void increaseDifficulty() {
+        if (level != Hard) {
+            level = static_cast<Level>(level + 1);
+        }
+    }
+
+    bool isMaxDifficulty() const {
+        return level == Hard;
+    }
    
 private:
     Level level;
