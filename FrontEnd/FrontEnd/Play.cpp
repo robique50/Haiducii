e7208a85d088dc682d5aa240c45130febaedd713
Haiduc_Robert <robert.haiduc@student.unitbv.ro>
2023-12-30 19:53:59 +0200

    Finished the lobby

--- FrontEnd/FrontEnd/Play.cpp
+++ FrontEnd/FrontEnd/Play.cpp
@@ -40,4 +40,43 @@ void Play::on_pushButton_joinGame_clicked()
     connect(reply, &QNetworkReply::finished, this, &Play::handleJoinLobbyResponse);
 }
 
+void Play::handleJoinLobbyResponse()
+{
+    QNetworkReply* reply = qobject_cast<QNetworkReply*>(sender());
+    if (!reply) {
+        QMessageBox::critical(this, "Error", "An unexpected error occurred.");
+        return;
+    }
+
+    if (reply->error()) {
+        QMessageBox::critical(this, "Error", "Failed to join the lobby: " + reply->errorString());
+        reply->deleteLater();
+        return;
+    }
+
+
+    QByteArray responseBytes = reply->readAll();
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(responseBytes);
+    if (!jsonResponse.isObject()) {
+        QMessageBox::critical(this, "Error", "Invalid response from server.");
+        reply->deleteLater();
+        return;
+    }
+
+    QJsonObject responseObject = jsonResponse.object();
+    QString message = responseObject.value("message").toString();
+    bool success = responseObject.value("success").toBool();
+
+    if (success) {
+
+        QMessageBox::information(this, "Lobby", "Successfully joined the lobby!");
+        // Aici puteți adăuga cod pentru a deschide fereastra lobby-ului sau pentru a actualiza interfața
+    }
+    else {
+        // Afisează un mesaj de eroare
+        QMessageBox::warning(this, "Failed", message);
+    }
+
+    reply->deleteLater();
+}
 
